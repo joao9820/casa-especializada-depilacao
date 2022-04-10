47ad382a8f784f71f568729392c4615d2c12a37c
joao9820 <joaovictorv9820@gmail.com>
2022-04-10 01:36:46 -0300

    Criação de layout padrão do site com as sessões e início do rodapé da página

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,10 +1,44 @@
 
-</div>
+    </div>
 
-<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
+    <footer>
+        <div class="container">
+
+            <h2 id="footerLogo">CASA ESPECIALIZADA EM DEPILAÇÃO</h2>
+
+            <div id="footerInfo">
+                <div class="info-adress">
+                    <p class="subtitle">Endereços</p>
+                    <div class="info-items">
+                        <div>
+                            <p><i class="fas fa-map-marker-alt"></i>QNB 05 Lote 39 - Loja 3/4 - Taguatinga Norte</p>
+                            <p><i class="fas fa-phone"></i>(61) 3352-4265</p>
+                            <p><i class="fab fa-whatsapp"></i>(61) 99934-7852</p>
+                        </div>
+                        <div class="mt-3">
+                            <p><i class="fas fa-map-marker-alt"></i>CBS 03 LOTE 07/08 - ED. São José - Taguatinga Sul</p>
+                            <p><i class="fas fa-phone"></i>(61) 3351-8488</p>
+                            <p><i class="fab fa-whatsapp"></i>(61) 99315-9867</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="info-items info-menu">
+                    <p class="subtitle">Menu</p>
+                    <div class="info-items"></div>
+                </div>
+                <div class="info-items info-social">
+                    <p class="subtitle">Social</p>
+                    <div class="info-items"></div>
+                </div>
+            </div>
+
+        </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
 
 
-</body>
+    </body>
 </html>
